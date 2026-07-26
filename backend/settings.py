0f28df973b1362405fdd3b6e875d01c909e7b2fa
AbhinavJadhav001 <abhinavjadhav591@gmail.com>
2026-07-26 19:00:54 +0530

    Fix ALLOWED_HOSTS for Railway (more flexible approch)

--- backend/settings.py
+++ backend/settings.py
@@ -43,11 +43,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env_bool("DEBUG", True)
 
-ALLOWED_HOSTS = [
-    "devjobboard-production.up.railway.app",
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
